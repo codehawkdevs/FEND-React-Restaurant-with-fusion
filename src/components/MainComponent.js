7bc88406a461967b5ea8df49efa7f08067a1cda0
Harsh Sahu <harsh.sahu@outlook.com>
2019-08-14 20:59:41 +0530

    routes added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,12 +7,18 @@ import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -23,16 +29,25 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
+        const DishWithId = () => {
+
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-                    <Route exact path="/contactus" component={contact} />
+                    <Route exact path="/contactus" component={Contact} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
